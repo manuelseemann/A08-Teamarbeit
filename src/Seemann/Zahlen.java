86fd6ac74aa8b81d75e534de207c1257d881bc36
Alexander Hasenberger <ahasenberger@student.tgm.ac.at>
2016-11-10 14:06:06 +0100

    Added Methods getSumme(), getMinimum(), getMaximum(), add()

--- src/Seemann/Zahlen.java
+++ src/Seemann/Zahlen.java
@@ -1,5 +1,36 @@
 package Seemann;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public class Zahlen {
+public class Zahlen implements IZahlen{
+	private ArrayList<Double> zahlen;
+	
+	public Zahlen(){
+		this.zahlen = new ArrayList<Double>();
+	}
 
+	@Override
+	public double getSumme() {
+		double summe = 0;
+		for(int i = 0; i < this.zahlen.size(); i++){
+			summe += this.zahlen.get(i);
+		}
+		return summe;
+	}
+
+	@Override
+	public double getMinimum() {
+		return Collections.min(this.zahlen);
+	}
+
+	@Override
+	public double getMaximum() {
+		return Collections.max(this.zahlen);
+	}
+
+	@Override
+	public void add(double value) {
+		this.zahlen.add(value);
+		
+	}
 }
