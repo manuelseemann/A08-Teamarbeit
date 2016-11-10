8aa2157625a6aba3ba3ad67e897bcdc78be24e4d
Alexander Hasenberger <ahasenberger@student.tgm.ac.at>
2016-11-10 15:09:52 +0100

    Added Comments

--- src/Seemann/Zahlen.java
+++ src/Seemann/Zahlen.java
@@ -2,13 +2,30 @@ package Seemann;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * Diese Klasse enthält Methoden zum Berechnen der Summe,
+ * zum Ausgaben des Größten/Kleinsten Werts und zum Hinzufügen einer Zahl.
+ * 
+ * @author Alexander Hasenberger
+ * @version 2016-11-10
+ */
 public class Zahlen implements IZahlen{
+	/**
+	 * Attribute
+	 */
 	private ArrayList<Double> zahlen;
 	
+	/**
+	 * Konstruktor
+	 */
 	public Zahlen(){
 		this.zahlen = new ArrayList<Double>();
 	}
 
+	/**
+	 * Diese Methode gibt die Summe der Elemente der Arraylist zurück.
+	 * @return die Summe der Elemente der Arraylist
+	 */
 	@Override
 	public double getSumme() {
 		double summe = 0;
@@ -18,16 +35,28 @@ public class Zahlen implements IZahlen{
 		return summe;
 	}
 
+	/**
+	 * Diese Methode gibt das kleinste Element der Arraylist zurück.
+	 * @return das kleinste Element der Arraylist
+	 */
 	@Override
 	public double getMinimum() {
 		return Collections.min(this.zahlen);
 	}
 
+	/**
+	 * Diese Methode gibt das größte Element der Arraylist zurück.
+	 * @return das größte Element der Arraylist
+	 */
 	@Override
 	public double getMaximum() {
 		return Collections.max(this.zahlen);
 	}
 
+	/**
+	 * Diese Methode fügt ein Element zur Arraylist hinzu.
+	 * @param value ist der Wert den das Element, welches hinzugefügt wird, haben soll
+	 */
 	@Override
 	public void add(double value) {
 		this.zahlen.add(value);
